40b7243f1c28a1e5545323e01b467f146370f2e6
Aryan Sharma <inbox.aryan@gmail.com>
2024-05-20 21:45:34 -0700

    Update MainActivity.kt

--- MyShoppingListApp/app/src/main/java/aryan/apps/myshoppinglistapp/MainActivity.kt
+++ MyShoppingListApp/app/src/main/java/aryan/apps/myshoppinglistapp/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -47,7 +48,9 @@ class MainActivity : ComponentActivity() {
                                 .fillMaxSize()
                                 .padding(16.dp)
                         ) {
+                            items(sItems) {
 
+                            }
                         }
                     }
                 }
